1a42a8073c8ede97d14807febf2d837bf73bb693
Xavi <xavi@geekshubs.com>
2019-07-16 15:22:02 +0200

    Implemented DoctorRepositoryTest

--- src/test/java/com/edicom/hisedicom/reposiotry/DoctorReposiotoryTest.java
+++ src/test/java/com/edicom/hisedicom/reposiotry/DoctorReposiotoryTest.java
@@ -2,14 +2,20 @@ package com.edicom.hisedicom.reposiotry;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.annotation.Order;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,6 +26,7 @@ import com.edicom.hisedicom.patients.domain.entities.Patient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DoctorReposiotoryTest {
 
 	
@@ -28,15 +35,40 @@ public class DoctorReposiotoryTest {
 private IDoctorDao doctorRepository;
 
 	@Test
+	
+	@Order(1)
 	public void injectedComponentsAreNotNull()
 	{
 		assertThat(doctorRepository).isNotNull();
 	}
 	
 	@Test
+	@Order(2)
 	public void getDoctorByCollegeNumber()
 	{
 		Doctor doctor = doctorRepository.getByCollegiateNumber("1");
 		assertThat(doctor).isNull();
+	}
+	
+	@Test
+	public void addDoctor()
+	{
+		Doctor doctor = new Doctor();
+		doctor.setName("Xavi");
+		doctor.setLastname("Rodriguez");
+		doctor.setCreatedAt(new Date());
+		doctor.setCollegiatenumber("1");
+		doctor.setSpecialty("General");
+		doctorRepository.save(doctor);
+		assertThat(doctor.getId()).isEqualTo(1);
+	}
+	
+	
+	@Test
+	@Order(4)
+	public void deleteDoctor()
+	{
+		int result = doctorRepository.deleteByCollegiateNumber("1");
+	     assertThat(result).isEqualTo(1);
 	}
 }
